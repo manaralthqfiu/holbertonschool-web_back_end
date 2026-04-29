2d3ed30d84ebbbb38d8f22283621ab5ba5738165
root <root@ip-10-42-135-133.ec2.internal>
2026-04-29 11:52:07 +0000

    add

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,8 @@
+def index_range(page, page_size):
+    """
+    Return a tuple (start_index, end_index) for pagination.
+    Page numbers are 1-indexed.
+    """
+    start = (page - 1) * page_size
+    end = page * page_size
+    return (start, end)
