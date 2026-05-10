3542b8d35161fa645956cea8546ccac1cffb74dc
manaralthqfiu <9vieo677@gmail.com>
2026-05-10 11:15:08 +0000

    add

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      const photo = results[0];
+      const user = results[1];
+
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
