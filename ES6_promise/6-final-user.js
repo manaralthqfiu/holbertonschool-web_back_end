f14df81cca635661d51ce8f75398bb1e114f2acc
manaralthqfiu <9vieo677@gmail.com>
2026-06-23 22:20:41 +0300

    add

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,5 +5,16 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName)
-  ]).then((results) => results);
+  ]).then((results) => results.map((item) => {
+    if (item.status === 'fulfilled') {
+      return {
+        status: item.status,
+        value: item.value
+      };
+    }
+    return {
+      status: item.status,
+      value: item.reason.toString().replace('Error: ', '')
+    };
+  }));
 }
