99a912d014f75e2cd50de55c5ac6f597011743fb
manaralthqfiu <9vieo677@gmail.com>
2026-06-24 13:54:46 +0300

    add

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,38 +1,23 @@
 import fs from 'fs';
 
-export function readDatabase(path) {
+export default function readDatabase(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', (err, data) => {
       if (err) {
-        reject(new Error('Cannot load the database'));
+        reject(err);
         return;
       }
 
-      const lines = data.split('\n').filter((line) => line.trim() !== '');
-      if (lines.length <= 1) {
-        resolve({});
-        return;
-      }
-
-      const students = lines.slice(1);
+      const lines = data.trim().split('\n');
       const fields = {};
 
-      students.forEach((student) => {
-        const studentData = student.split(',');
-        if (studentData.length >= 4) {
-          const firstName = studentData[0].trim();
-          const field = studentData[3].trim();
-
-          if (!fields[field]) {
-            fields[field] = [];
-          }
-          fields[field].push(firstName);
-        }
-      });
+      for (let i = 1; i < lines.length; i += 1) {
+        const [firstname, lastname, age, field] = lines[i].split(',');
+        if (!fields[field]) fields[field] = [];
+        fields[field].push(firstname);
+      }
 
       resolve(fields);
     });
   });
 }
-
-export default readDatabase;
