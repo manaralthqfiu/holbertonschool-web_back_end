719dc6fa59ee2e4cc231bc8e6c28cbf61d28ac86
manaralthqfiu <9vieo677@gmail.com>
2026-06-24 13:37:50 +0300

    add..

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,12 +1,6 @@
-const http = require('http');
-
-const PORT = 1245;
-
-const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Holberton School!');
-});
-
-app.listen(PORT);
-
-module.exports = app;
+export default function signUpUser(firstName, lastName) {
+  return Promise.resolve({
+    firstName,
+    lastName,
+  });
+}
