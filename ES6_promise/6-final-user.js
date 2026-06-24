9d419033015a20af7f457e819efa3c3169a5dd28
manaralthqfiu <9vieo677@gmail.com>
2026-06-24 13:14:30 +0300

    add

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,17 +1,23 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((results) => results.map((result) => {
-    if (result.status === 'rejected') {
+  ]).then((results) =>
+    results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return {
+          status: result.status,
+          value: result.value,
+        };
+      }
+
       return {
-        status: 'rejected',
-        value: result.reason.replace('Error: ', ''),
+        status: result.status,
+        value: `${result.reason}`,
       };
-    }
-    return result;
-  }));
+    })
+  );
 }
